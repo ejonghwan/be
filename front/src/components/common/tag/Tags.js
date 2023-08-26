692fff94944f70518af953656d0ff9ae1fb18e63
ejonghwan <sun87-1@daum.net>
2023-08-26 19:05:34 +0900

    태그지우기 기능 완성.

--- front/src/components/common/tag/Tags.js
+++ front/src/components/common/tag/Tags.js
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
+import { PiXCircleDuotone } from "react-icons/pi";
 import './Tags.css';
 
 // to 링크는 나중에 태그 누르면 검색페이지로 가게 할때 작업
-const Tags = ({ className = '', tags = [], isLink = false }) => {
+const Tags = ({ className = '', tags = [], isLink = false, handleDelete }) => {
+    console.log(tags)
     return (
         <ul className={`tag_wrap ${className}`}>
             {tags.map((tag, idx) => {
                 return (
                     <li key={idx}>
-                        {isLink ? <Link to={`/${tag.categoryName}`}>{`# ${tag.categoryName}`}</Link> : `# ${tag.categoryName}`}
+                        {isLink ? 
+                            <Link to={`/${tag}`}>{`# ${tag}`}</Link> 
+                            : 
+                            (<div className='no_link'>
+                                {tag}
+                                <button type="button" className='button_reset button_delete' onClick={() => handleDelete(tag)}>
+                                    <span className='blind'>{`${tag} 없애기`}</span>
+                                </button>
+                            </div>)
+                        }
                     </li>
                 )
             })}
--- front/src/components/project/CreateProjectDetail.js
+++ front/src/components/project/CreateProjectDetail.js
@@ -31,13 +31,14 @@ const CreateProjectDetail = () => {
     // likeUser x
 
     const [projectImages, setProjectImages] = useState(0);
-    const [categoryValue, setCategoryValue] = useState('');
-    const [joinUserValue, setJoinUserValue] = useState('');
+    const [categoryValue, setCategoryValue] = useState(''); 
+    const [joinUserValue, setJoinUserValue] = useState(''); // 인풋값
+    const [joinUserList, setJoinUserList] = useState([]); //뿌리기 위해 여기서만 사용
     const [isUserSearchResult, setIsUserSearchResult] = useState(false)
-    const [val, setVal] = useState({
+    const [submitData, setSubmitData] = useState({ 
         title: '',
         content: '',
-        categorys: [{categoryName: 'ㅋㅋ'}, {categoryName: 'ghg'}],
+        categorys: [{categoryName: 'ㅋㅋ'}, {categoryName: 'aa'}, {categoryName: 'bb'}, {categoryName: 'cc'}],
         joinUser: [],
         projectPublic: false,
     });
@@ -45,9 +46,10 @@ const CreateProjectDetail = () => {
 
     const handleValuesChange = e => {
         const {name, value} = e.target;
-        setVal({...val, [name]: value})
+        setSubmitData({...submitData, [name]: value})
     }
 
+    // 카테고리 관련 이벤트
     const handleCategoryReset = () => setCategoryValue('');
     const handleCategoryClick = useCallback(() => {
         let categoryResult = categoryValue.replace(/ /g,"").split('#').filter(item => {
@@ -57,25 +59,26 @@ const CreateProjectDetail = () => {
         for(let i = 0; i < categoryResult.length; i++) {
             inCategoryName.push({ categoryName: categoryResult[i] })
         }
-        setVal(prev => {
-            return {...prev, categorys: [...categorys, ...inCategoryName]}
-        })
+        setSubmitData(prev => ({...prev, categorys: [...prev.categorys, ...inCategoryName]}))
         setCategoryValue('')
     }, [categoryValue])
 
+    const handleTagDelete = tagName => {
+        setSubmitData(prev => ({ ...prev, categorys: prev.categorys.filter(tag => tag.categoryName !== tagName )}))
+    }
+
    
 
+    // 검색 관련 이벤트
     const handleSearchCange = e => {
         setJoinUserValue(e.target.value)
         handleJoinUserSearch(e.target.value) 
         //그리고 useCallback 안에서는 state를 구독하지 않기 때문에 변화 값을 인자로 넘겨줘야함 
     }
 
-
     // 유저 검색
     const handleJoinUserSearch = useCallback(_debounce(async (userName) => {
         // useCallback을 사용하면서 joinUserValue를 구독하지 않아, 서치인풋이 리렌더링이 되어도 이 함수의 주소값의 변화가 없음. 중요. debounce 사용하면서 디바운스 계속 호출되던 이슈. 
-        console.log('de')
         try {
             if(userName === '') return setIsUserSearchResult(false);
             console.log(userName)
@@ -87,26 +90,42 @@ const CreateProjectDetail = () => {
         setIsUserSearchResult(true)
     }, 500), [])
 
-
      // 유저검색창 엑스버튼
      const handleUserValueReset = () => {
         setJoinUserValue('');
         setIsUserSearchResult(false);
      };
     
+     const handleAddFriend = ({ name, _id }) => () => {
+        for(let i = 0; i < joinUserList.length; i++) {
+            if(submitData.joinUser[i].match(_id)) return alert('이미 추가한 친구입니다.')
+        }
+        setSubmitData(prev => ({ ...prev, joinUser: prev.joinUser.concat(_id) }));
+        setJoinUserList(prev => [...prev, { name: name, _id: _id }]);
+        setJoinUserValue('');
+        setIsUserSearchResult(false);
+     }
+
+    const handleJoinUserDelete = tagName => {
+        let getId = joinUserList.filter(user => user.name === tagName)[0]._id;
+        setJoinUserList(prev => prev.filter(user => user.name !== tagName));
+        setSubmitData(prev => ({ ...prev, joinUser: prev.joinUser.filter(_id => _id !== getId) }))
+
+    }
+
 
     // 습관 생성
-    const handleCreateProjectSubmit = e => {
+    const handleCreateProjectSubmit = e => { 
         e.preventDefault();
     }
 
     const handleIconClick = idx => setProjectImages(idx);
-    const { title, content, projectPublic, categorys } = val;
+    const { title, content, projectPublic, categorys } = submitData;
 
 
     useEffect(() => {
-        // console.log(val)
-    }, [val])
+        console.log(submitData)
+    }, [submitData])
 
     
     
@@ -173,7 +192,11 @@ const CreateProjectDetail = () => {
                         ) : (
                             <ul className='search_result_user'>
                                 {SearchState.userSearch?.map(((user, idx) => <li key={idx} className='search_result_user_item'>{
-                                    <button type='button' className='button_reset' title={`${user.id}님 친구추가`}>
+                                    <button type='button' 
+                                        className='button_reset' 
+                                        title={`${user.id}님 친구추가`} 
+                                        onClick={handleAddFriend({ name: user.name, _id: user._id })}
+                                    >
                                         <img src={`${process.env.REACT_APP_BACKEND_HOST}/uploads/${user.profileImage.key}`} alt="" className='user_img'/>
                                         <strong className='user_name'>{user.name}</strong>
                                         <span className='user_id'>{user.id}</span>
@@ -183,8 +206,13 @@ const CreateProjectDetail = () => {
                             </ul>
                         )}
                         {!SearchState.loading && SearchState.userSearch.length === 0 && <NoData icon={<PiSmileyXEyesDuotone />} title={"검색한 친구는 회원이 아닙니다."} subText={" 다시 검색해보세요."}/>}
-                       
                     </Search>
+                    <div>추가한 친구 
+                        {joinUserList?.map(user => <div key={user._id}>{user.name}</div>)}
+                    </div>
+                    <div className='category_wrap gapt_10'>
+                        <Tags tags={joinUserList?.map(user => user.name)} isLink={false} handleDelete={handleJoinUserDelete}/>
+                    </div>
                 </div>
                 <div className='gap_30'>
                     <Search 
@@ -201,9 +229,9 @@ const CreateProjectDetail = () => {
                         onChange={e => setCategoryValue(e.target.value)}
                         handleInputReset={handleCategoryReset}
                     />
-                    <ul className='category_wrap gapt_10'>
-                        <Tags tags={val.categorys.map(tag => tag)} isLink={false}/>
-                    </ul>
+                    <div className='category_wrap gapt_10'>
+                        <Tags tags={submitData.categorys.map(tag => tag.categoryName)} isLink={false} handleDelete={handleTagDelete}/>
+                    </div>
                 </div>
 
                 <div className='gap_30'>
