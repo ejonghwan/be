d3fdf638e56a43ce112d90b38f7ae65e1f534dc3
ejonghwan <sun87-1@daum.net>
2023-09-24 00:43:32 +0900

    로딩 중일땐 프로텍트 페이지  안보이게 처리

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -28,10 +28,10 @@ const RoutesPage = () => {
     const { state } = useContext(UserContext);
     return (
         <Layout>
-            <Routes>
+            {/* <Routes>
                 <Route path='/' element={<Home page={'메인페이지'}/>}></Route>
-                <Route path='/projectlist' element={state.isLogged ? <About page={'친구들 습관보기'} /> : <Protected page={'친구들 습관보기'}/>}></Route>
-                <Route path='/profile' element={state.isLogged ? <Profile page={'내 정보'} /> : <Protected page={'내 정보'} />}></Route>
+                <Route path='/projectlist' element={!state.loadUserLoading && state.isLogged  ? <About page={'친구들 습관보기'} /> : <Protected page={'친구들 습관보기'}/>}></Route>
+                <Route path='/profile' element={!state.loadUserLoading && state.isLogged  ? <Profile page={'내 정보'} /> : <Protected page={'내 정보'} />}></Route>
                 <Route path='/login' element={ <Login page={'로그인'}/>}></Route>
                 <Route path='/signup' element={<Signup page={'회원가입'} />}></Route>
                 <Route path='/signuppage' element={<SignupPage page={'회원가입'} />}></Route>
@@ -40,19 +40,44 @@ const RoutesPage = () => {
                 <Route path='/changepassword' element={<ChangePassword page={'비밀번호 변경'} />}></Route>
 
 
+                <Route path='/createproject' element={!state.loadUserLoading && state.isLogged  ? <CreateProject page={'새 습관 만들기'} /> : <Protected page={'새 습관 만들기'} />}></Route>
+                <Route path='/project/detail/:_id' element={!state.loadUserLoading && state.isLogged  ? <ProjectDetail page={'습관 상세보기'} /> : <Protected page={'습관 상세보기'} />}></Route>
 
-                <Route path='/createproject' element={state.isLogged ? <CreateProject page={'새 습관 만들기'} /> : <Protected page={'새 습관 만들기'} />}></Route>
-                <Route path='/project/detail/:_id' element={state.isLogged ? <ProjectDetail page={'습관 상세보기'} /> : <Protected page={'습관 상세보기'} />}></Route>
+                
+                <Route path='/write/detail/:_id' element={!state.loadUserLoading && state.isLogged  ? <Write page={'글 상세보기'} /> : <Protected page={'글 상세보기'} />}></Route> 
+                <Route path='/write/mylist' element={!state.loadUserLoading && state.isLogged  ? <MyWritesList page={'내 글'} /> : <Protected page={'내 글'} />}></Route>
 
-                {/* <Route path='/write/list/user/:_id' element={state.isLogged ? <WriteDetail page={'글 모음'} /> : <Protected page={'글 모음'} />}></Route> */}
-                <Route path='/write/detail/:_id' element={state.isLogged ? <Write page={'글 상세보기'} /> : <Protected page={'글 상세보기'} />}></Route> 
-                <Route path='/write/mylist' element={state.isLogged ? <MyWritesList page={'내 글'} /> : <Protected page={'내 글'} />}></Route>
+                <Route path='/project:id' element={<Project page={'습관보기'} />}></Route>
+                <Route path='/error' element={<Error page={'에러'}/>}></Route>
+                <Route path='/style/:id' element={<StyleGuide />}></Route>
+            </Routes> */}
 
+             <Routes>
+                <Route path='/' element={<Home page={'메인페이지'}/>}></Route>
+                <Route path='/projectlist' element={!state.loadUserLoading !== null && state.isLogged && <About page={'친구들 습관보기'} />}></Route>
+                <Route path='/profile' element={!state.loadUserLoading !== null && state.isLogged  ? <Profile page={'내 정보'} /> : <Protected page={'내 정보'} />}></Route>
+                <Route path='/login' element={ <Login page={'로그인'}/>}></Route>
+                <Route path='/signup' element={<Signup page={'회원가입'} />}></Route>
+                <Route path='/signuppage' element={<SignupPage page={'회원가입'} />}></Route>
+                <Route path='/findid' element={<FindId page={'아이디 찾기'} />}></Route>
+                <Route path='/findpassword' element={<FindPassword page={'비밀번호 찾기'} />}></Route>
+                <Route path='/changepassword' element={<ChangePassword page={'비밀번호 변경'} />}></Route>
+
+
+                <Route path='/createproject' element={!state.loadUserLoading !== null && state.isLogged  ? <CreateProject page={'새 습관 만들기'} /> : <Protected page={'새 습관 만들기'} />}></Route>
+                <Route path='/project/detail/:_id' element={!state.loadUserLoading !== null && state.isLogged  ? <ProjectDetail page={'습관 상세보기'} /> : <Protected page={'습관 상세보기'} />}></Route>
+
+                
+                <Route path='/write/detail/:_id' element={!state.loadUserLoading !== null && state.isLogged  ? <Write page={'글 상세보기'} /> : <Protected page={'글 상세보기'} />}></Route> 
+                <Route path='/write/mylist' element={!state.loadUserLoading !== null && state.isLogged  ? <MyWritesList page={'내 글'} /> : <Protected page={'내 글'} />}></Route>
 
                 <Route path='/project:id' element={<Project page={'습관보기'} />}></Route>
                 <Route path='/error' element={<Error page={'에러'}/>}></Route>
                 <Route path='/style/:id' element={<StyleGuide />}></Route>
             </Routes>
+
+
+            {/* <Route path='/write/list/user/:_id' element={!state.loadUserLoading && state.isLogged  ? <WriteDetail page={'글 모음'} /> : <Protected page={'글 모음'} />}></Route> */}
         </Layout>
     );
 };
--- front/src/pages/protected/protectedPage.js
+++ front/src/pages/protected/protectedPage.js
@@ -1,14 +1,24 @@
 import LoginForm from '../../components/user/LoginForm';
 import FindList from '../../components/user/FindList';
+import { Fragment, useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 
 const ProtectedPage = ({ page }) => {
+
+    const {state, dispatch} = useContext(UserContext);
+
     return (
-        <div className='b_conts'>
-            <h2>{page}</h2>
-            <p className='align_c gap_50'>{page} 페이지를 보시려면 로그인해주세요.</p>
-            <LoginForm />
-            <FindList />
-        </div>
+        <Fragment>
+            {/* 로딩 중일땐 아래 jsx 안보이게 처리 */}
+            {!state.loadUserLoading && (
+                <div className='b_conts'>
+                    <h2>{page}</h2>
+                    <p className='align_c gap_50'>{page} 페이지를 보시려면 로그인해주세요.</p>
+                    <LoginForm />
+                    <FindList />
+                </div>
+            )}
+        </Fragment>
     );
 };
 
