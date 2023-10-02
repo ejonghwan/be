aa5019ecde2c8c3d3edf994efe64273764ee8240
ejonghwan <sun87-1@daum.net>
2023-10-03 03:54:51 +0900

    new date 삭제

--- back/routes/email.js
+++ back/routes/email.js
@@ -54,7 +54,7 @@ router.get('/signup', async (req, res) => {
 
         const encode = encodeURIComponent(match.email)
         const query = querystring.stringify({ valid: true, email: encode})
-        res.cookie('signup', true, { expires: new Date(Date.now() + 1000 * 60 * 15)}).redirect(`${process.env.DOMAIN}/signup?${query}`)
+        res.cookie('signup', true).redirect(`${process.env.DOMAIN}/signup?${query}`)
         
        
     } catch(err) {
