75181be4b0331ea23367487d114505ce22e9f7da
ejonghwan <sun87-1@daum.net>
2023-10-03 03:00:57 +0900

    백엔드 쿠키 테스트

--- back/routes/email.js
+++ back/routes/email.js
@@ -54,8 +54,9 @@ router.get('/signup', async (req, res) => {
 
         const encode = encodeURIComponent(match.email)
         const query = querystring.stringify({ valid: true, email: encode})
-        res.redirect(`${process.env.DOMAIN}/signup?${query}`)
         res.cookie('signup', true, { expires: new Date(Date.now() + 1000 * 60 * 15)})
+        res.redirect(`${process.env.DOMAIN}/signup?${query}`)
+        
        
     } catch(err) {
         console.error(err)
