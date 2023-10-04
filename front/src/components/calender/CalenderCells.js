0e95832806945d3b0ec393c149362908633d27cc
ejonghwan <sun87-1@daum.net>
2023-10-05 01:16:56 +0900

    모바일 확대 막기 추가 및 달력 버그 해결

--- front/src/components/calender/CalenderCells.js
+++ front/src/components/calender/CalenderCells.js
@@ -32,7 +32,7 @@ const CalenderCells = ({ currentMonth, selectedDate, onDateClick, project }) => 
                         className={`col cell ${
                             !isSameMonth(day, monthStart) ? 'disabled' : isSameDay(day, selectedDate)
                                 ? 'selected' : format(currentMonth, 'M') !== format(day, 'M')
-                                ? 'not-valid' : 'valid'} ${project.constructorUser.days?.map(userDay => isSameDay(day, new Date(userDay.date.replaceAll('-', '/'))) && userDay.count >= 0 ? `constructor` : null).join('')} ${project.instanceUser?.map(user => user.days?.map((userDay, idx) => isSameDay(day, new Date(userDay.date.replaceAll('-', '/'))) && userDay.count >= 0 ? `constructor` : null).join(' ')).join(' ')} ${isSameDay(day, new Date()) ? `today` : ``}`}
+                                ? 'not-valid' : 'valid'} ${project.constructorUser.days?.map(userDay => isSameDay(day, new Date(userDay.date.replaceAll('-', '/'))) && userDay.count >= 0 ? `constructor instance` : null).join('')} ${project.instanceUser?.map(user => user.days?.map((userDay, idx) => isSameDay(day, new Date(userDay.date.replaceAll('-', '/'))) && userDay.count >= 0 ? `instance constructor` : null).join(' ')).join(' ')} ${isSameDay(day, new Date()) ? `today` : ``}`}
                         key={day}
                         // onClick={() => onDateClick(parse(cloneDay))}
                         onClick={() => onDateClick(cloneDay, new Date(cloneDay))}
