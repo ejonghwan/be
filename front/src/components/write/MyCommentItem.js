d637c259a765a8fa487cc18dfd0ed754f2ad0d7a
ejonghwan <sun87-1@daum.net>
2024-05-20 22:46:58 +0900

    내 코멘트 안나오는 이슈 해결

--- front/src/components/write/MyCommentItem.js
+++ front/src/components/write/MyCommentItem.js
@@ -41,15 +41,14 @@ const MyCommentItem = ({ comments = [] }) => {
                         </div>
                     ) : (
                         <Fragment>
-                            { console.log('coco??', comment) }
-                            {/* <div className='project_image'>{IconData[comment.writeId.project?._id.projectImages]}</div>
+                            <div className='project_image'>{IconData[comment.writeId?.project._id.projectImages]}</div>
                             <div>
-                                <div className='comments_list_content word_ellip_3'>{comment.content}</div>
-                                <Link to={`/write/detail/${comment.writeId._id}`} className='comments_list_write'>
-                                    <span className='tit'>{comment.writeId.title}</span> 글에 남긴 댓글
+                                <div className='comments_list_content word_ellip_3'>{comment?.content}</div>
+                                <Link to={`/write/detail/${comment.writeId?._id}`} className='comments_list_write'>
+                                    <span className='tit'>{comment.writeId?.title}</span> 글에 남긴 댓글
                                 </Link>
-                                <Link to={`/project/detail/${comment.writeId.project._id._id}`} className='comments_list_title'>
-                                    <span className='tit'>{comment.writeId.project._id.title}</span> 중에서...
+                                <Link to={`/project/detail/${comment.writeId?.project._id._id}`} className='comments_list_title'>
+                                    <span className='tit'>{comment.writeId?.project._id.title}</span> 중에서...
                                 </Link>
                                 <div className='comments_date_wrap'>
                                     <p>{changeViewDate(comment.createdAt, 'day')}</p>
@@ -57,7 +56,7 @@ const MyCommentItem = ({ comments = [] }) => {
                             </div>
                             <Button className={'button_reset button_delete2'} onClick={() => handleDeleteComment(comment)}>
                                 <span className='blind'>이 댓글 삭제</span>
-                            </Button> */}
+                            </Button>
                         </Fragment>
                     )}
                 </li>
